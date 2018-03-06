fa78bbe0f67920741609ae726afb0144691db309
Melanie Macken <m.macken@otmail.nl>
2018-03-06 20:23:46 +0100

    first commit

---
+++ Extensions/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    /* Every time the window is scrolled ... */
+$(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.hideMe').each(function (i) {
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 1500);
+
+            }
+
+        });
+
+    });
+
+});
+
+
+$(window).scroll(function () {
+
+ $('.subplz').each(function (i) {
+
+           var bottom_of_object = $(this).position().top + $(this).outerHeight();
+           var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+           /* If the object is completely visible in the window, fade it it */
+           if (bottom_of_window > bottom_of_object) {
+
+             jQuery('.subplz').removeClass( "hideSub" ).addClass("visible animated slideInDown");
+
+
+
+
+
+ };
+});
+});
